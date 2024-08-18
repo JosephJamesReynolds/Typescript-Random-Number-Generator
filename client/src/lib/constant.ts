a1586bec37a9d5195250c2b8c7770040e14e8aa7
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2024-08-18 16:13:10 -0700

    Finished TS mini project

---
+++ client/src/lib/constant.ts
@@ -0,0 +1,4 @@
+import abi from "./abi/Random.json";
+
+export const contractAbi = abi;
+export const contractAddress = "0x97777D57c7E61E5E0E6E7bD7Bd075d9eA7c34760";
--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,14 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+// Smart contract deployed to 0x97777D57c7E61E5E0E6E7bD7Bd075d9eA7c34760
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: process.env.ALCHEMY_API_KEY,
-      accounts: [
-        process.env.PRIVATE_KEY !== undefined ? process.env.PRIVATE_KEY : "",
-      ],
+      url:
+        process.env.ALCHEMY_API_KEY !== undefined
+          ? `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+          : "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
